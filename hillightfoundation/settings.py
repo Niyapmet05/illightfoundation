de6171cd2d1468c46c052ba0516d6d2bf3cbfe59
niyapmet05 <niyomurengeziaphrodis@gmail.com>
2021-06-15 11:54:25 +0200

    Correct allowed host

--- hillightfoundation/settings.py
+++ hillightfoundation/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'n$mggde34yu_*y2h6+en64u39gnhd-
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') == 'True'
 DEBUG = True
 
-ALLOWED_HOSTS = ['stark-scrubland-81172.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['hillightfoundation.herokuapp.com','127.0.0.1']
 
 
 # Application definition
